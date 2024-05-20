c32f96fdd40e28e14f12381934a946a142d7a307
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-05-20 15:22:45 +0300

    server

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 
-// server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
 app.listen(3000, () => console.log('Server Running'));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: '********@gmail.com',
-    pass: '',
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASSWORD,
   },
 });
 
@@ -29,13 +27,13 @@ contactEmail.verify((error) => {
 });
 
 router.post('/contact', (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+  const name = req.body.firstName + ' ' + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
-    from: name,
-    to: '********@gmail.com',
+    from: email,
+    to: process.env.MAILTRAP_URL,
     subject: 'Contact Form Submission - BeerKing',
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
@@ -50,54 +48,3 @@ router.post('/contact', (req, res) => {
     }
   });
 });
-
-// const express = require('express');
-// const router = express.Router();
-// const cors = require('cors');
-// const nodemailer = require('nodemailer');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use('/', router);
-// app.listen(3000, () => console.log('Server Running'));
-
-// const contactEmail = nodemailer.createTransport({
-//   host: 'sandbox.smtp.mailtrap.io',
-//   port: 2525,
-//   auth: {
-//     user: '0d3002e0dba0b8',
-//     pass: '********822f',
-//   },
-// });
-
-// contactEmail.verify((error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Ready to Send');
-//   }
-// });
-
-// router.post('/contact', (req, res) => {
-//   const name = req.body.firstName + ' ' + req.body.lastName;
-//   const email = req.body.email;
-//   const message = req.body.message;
-//   const phone = req.body.phone;
-//   const mail = {
-//     from: email,
-//     to: 'your_mailtrap_inbox@inbox.mailtrap.io',
-//     subject: 'Contact Form Submission - Portfolio',
-//     html: `<p>Name: ${name}</p>
-//            <p>Email: ${email}</p>
-//            <p>Phone: ${phone}</p>
-//            <p>Message: ${message}</p>`,
-//   };
-//   contactEmail.sendMail(mail, (error) => {
-//     if (error) {
-//       res.json(error);
-//     } else {
-//       res.json({ code: 200, status: 'Message Sent' });
-//     }
-//   });
-// });
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-import { Container, Row, Col } from 'react-bootstrap';
-// import { MailchimpForm } from './MailchimpForm';
+import { Container } from 'react-bootstrap';
+
 import crown from '../assets/img/icons/crown.png';
 import telegram from '../assets/img/icons/telegram.svg';
 import viber from '../assets/img/icons/viber.svg';
@@ -9,40 +9,46 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
-          <Col size={12} sm={6}>
-            <img className="navbar-logo" src={crown} alt="logo" />
-            <h3>BEERKING</h3>
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="footer-social-icon">
-              <a
-                href="https://web.telegram.org/k/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={telegram} alt="telegram" />
-              </a>
+        <div className="footer-wrap">
+          <img className="navbar-logo" src={crown} alt="logo" />
+          <h3>BEERKING</h3>
+          <ul className="footer-list">
+            <li className="footer-item"></li>
+            <li className="footer-item">
+              <div className="footer-social">
+                <p className="footer-text">Замовити онлайн</p>
+                <div className="footer-social-icon">
+                  <a
+                    href="https://web.telegram.org/k/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={telegram} alt="telegram" />
+                  </a>
 
-              <a
-                href="https://www.viber.com/ua/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={viber} alt="viber" />
-              </a>
-              <a
-                href="https://www.instagram.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={instagram} alt="instagram" />
-              </a>
-            </div>
-            <p> CopyRight 2024. All Rights Reserved</p>
-          </Col>
-        </Row>
+                  <a
+                    href="https://www.viber.com/ua/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={viber} alt="viber" />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={instagram} alt="instagram" />
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <p className="footer-text-right">
+            {' '}
+            CopyRight 2024. All Rights Reserved
+          </p>
+        </div>
       </Container>
     </footer>
   );
