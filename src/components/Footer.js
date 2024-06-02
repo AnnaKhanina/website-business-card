e0e8a19fd697cd92498d50ddb558c6873cf2ac09
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-06-02 14:54:53 +0300

    add footer item test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -126,93 +126,92 @@ export const Footer = () => {
     // </footer>
     <footer className="footer">
       <Container>
-        <div className="footer-wrap">
-          <img className="navbar-logo" src={crown} alt="logo" />
-          <h3>BEERKING</h3>
-          <ul className="footer-list">
-            <li className="footer-item">
-              <div className="footer-social">
-                <form id="subscribe-form" onSubmit={handleSubmit}>
-                  <label htmlFor="email" className="label-text">
-                    Підписуйтесь на новини
-                  </label>
-                  <div className="footer-form">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Введіть Ваш email"
-                      required
-                      className="footer-input"
-                    />
-                    <button
-                      type="submit"
-                      disabled={loading}
-                      className="footer-btn"
-                    >
-                      <span className="navbar-btn-text">
-                        {loading ? (
-                          <Spinner as="span" animation="border" size="sm" />
-                        ) : (
-                          'Підписатися'
-                        )}
-                      </span>
-                    </button>
-                  </div>
-                </form>
-              </div>
+        <img className="navbar-logo" src={crown} alt="logo" />
+        <h3>BEERKING</h3>
+        <div className="footer-social">
+          <p className="footer-text">Ми в соцмережах</p>
+          <ul className="footer-social-list">
+            <li className="footer-social-item">
+              <a
+                href="https://web.telegram.org/k/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={telegram} alt="telegram" />
+              </a>
             </li>
-            <li className="footer-item">
-              <div className="footer-social">
-                <p className="footer-text">Ми в соцмережах</p>
-                <div className="footer-social-icon">
-                  <a
-                    href="https://web.telegram.org/k/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src={telegram} alt="telegram" />
-                  </a>
-
-                  <a
-                    href="https://www.viber.com/ua/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src={viber} alt="viber" />
-                  </a>
-                  <a
-                    href="https://www.instagram.com/beer_king_market?igsh=MTJmZHRlMmd1Y2Ywag=="
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src={instagram} alt="instagram" />
-                  </a>
-                </div>
-              </div>
+            <li className="footer-social-item">
+              <a
+                href="https://www.viber.com/ua/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={viber} alt="viber" />
+              </a>
             </li>
-            <li className="footer-item">
-              <div className="footer-section">
-                <p className="footer-text">
-                  <a href="/about" className="footer-link">
-                    Про Нас
-                  </a>
-                </p>
-                <p className="footer-text">
-                  <a href="/privacy-policy" className="footer-link">
-                    Політика Конфіденційності
-                  </a>
-                </p>
-              </div>
+            <li className="footer-social-item">
+              <a
+                href="https://www.instagram.com/beer_king_market?igsh=MTJmZHRlMmd1Y2Ywag=="
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={instagram} alt="instagram" />
+              </a>
             </li>
           </ul>
-          <p className="footer-text-right">
-            {' '}
-            CopyRight 2024. All Rights Reserved
-          </p>
         </div>
+
+        <div className="footer-subscribe">
+          <form id="subscribe-form" onSubmit={handleSubmit}>
+            <label htmlFor="email" className="label-text">
+              Підписуйтесь на новини
+            </label>
+            <div className="footer-form">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Введіть Ваш email"
+                required
+                className="footer-input"
+              />
+              <button type="submit" disabled={loading} className="footer-btn">
+                <span className="navbar-btn-text">
+                  {loading ? (
+                    <Spinner as="span" animation="border" size="sm" />
+                  ) : (
+                    'Підписатися'
+                  )}
+                </span>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="footer-section">
+          <ul className="footer-section-list">
+            <li className="footer-section-item">
+              <p className="footer-text">
+                <a href="/about" className="footer-link">
+                  Про Нас
+                </a>
+              </p>
+            </li>
+            <li className="footer-section-item">
+              <p className="footer-text">
+                <a href="/privacy-policy" className="footer-link">
+                  Політика Конфіденційності
+                </a>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <p className="footer-text-right">
+          {' '}
+          CopyRight 2024. All Rights Reserved
+        </p>
       </Container>
     </footer>
   );
