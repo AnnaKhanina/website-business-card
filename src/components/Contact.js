2746c7ba666ef8bbccb489655365283b1bdb9fb3
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-06-02 14:16:22 +0300

    add footer item

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,165 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { Container, Row, Col, Spinner } from 'react-bootstrap';
-
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import contactImg from '../assets/img/logo-beerking1.png';
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
-
-// export const Contact = () => {
-//   const formInitialDetails = {
-//     firstName: '',
-//     lastName: '',
-//     email: '',
-//     phone: '',
-//     message: '',
-//   };
-
-//   const [formDetails, setFormDetails] = useState(formInitialDetails);
-//   const [buttonText, setButtonText] = useState('Відправити');
-//   const [loading, setLoading] = useState(false);
-
-//   const onFormUpdate = (category, value) => {
-//     setFormDetails({
-//       ...formDetails,
-//       [category]: value,
-//     });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setLoading(true);
-//     setButtonText('Відправлення...');
-//     try {
-//       let response = await axios.post('/api/contact', formDetails);
-//       setButtonText('Відправити');
-//       setFormDetails(formInitialDetails);
-//       if (response.status === 200) {
-//         toast.success('Повідомлення успішно надіслано', {
-//           containerId: 'main-toast',
-//         });
-//       } else {
-//         toast.error('Щось пішло не так, спробуйте пізніше.', {
-//           containerId: 'main-toast',
-//         });
-//       }
-//     } catch (error) {
-//       setButtonText('Відправити');
-//       toast.error('Щось пішло не так, спробуйте пізніше.', {
-//         containerId: 'main-toast',
-//       });
-//     } finally {
-//       setLoading(false);
-//       setButtonText('Відправити');
-//     }
-//   };
-
-//   return (
-//     <section className="contact" id="connect">
-//       <Container>
-//         <Row className="align-items-center">
-//           <Col size={12} md={6}>
-//             <TrackVisibility>
-//               {({ isVisible }) => (
-//                 <img
-//                   className={
-//                     isVisible ? 'animate__animated animate__zoomIn' : ''
-//                   }
-//                   src={contactImg}
-//                   alt="Contact Us"
-//                 />
-//               )}
-//             </TrackVisibility>
-//           </Col>
-//           <Col size={12} md={6}>
-//             <TrackVisibility>
-//               {({ isVisible }) => (
-//                 <div
-//                   className={
-//                     isVisible ? 'animate__animated animate__fadeIn' : ''
-//                   }
-//                 >
-//                   <h2>Будемо на зв'язку</h2>
-//                   <form onSubmit={handleSubmit}>
-//                     <Row>
-//                       <Col size={12} sm={6} className="px-1">
-//                         <input
-//                           type="text"
-//                           value={formDetails.firstName}
-//                           placeholder="Ім'я"
-//                           onChange={(e) =>
-//                             onFormUpdate('firstName', e.target.value)
-//                           }
-//                         />
-//                       </Col>
-//                       <Col size={12} sm={6} className="px-1">
-//                         <input
-//                           type="text"
-//                           value={formDetails.lastName}
-//                           placeholder="Прізвище"
-//                           onChange={(e) =>
-//                             onFormUpdate('lastName', e.target.value)
-//                           }
-//                         />
-//                       </Col>
-//                       <Col size={12} sm={6} className="px-1">
-//                         <input
-//                           type="email"
-//                           value={formDetails.email}
-//                           placeholder="Email"
-//                           onChange={(e) =>
-//                             onFormUpdate('email', e.target.value)
-//                           }
-//                         />
-//                       </Col>
-//                       <Col size={12} sm={6} className="px-1">
-//                         <input
-//                           type="tel"
-//                           value={formDetails.phone}
-//                           placeholder="Телефон"
-//                           onChange={(e) =>
-//                             onFormUpdate('phone', e.target.value)
-//                           }
-//                         />
-//                       </Col>
-//                       <Col size={12} className="px-1">
-//                         <textarea
-//                           rows="6"
-//                           value={formDetails.message}
-//                           placeholder="Повідомлення"
-//                           onChange={(e) =>
-//                             onFormUpdate('message', e.target.value)
-//                           }
-//                         ></textarea>
-//                         <button
-//                           className="contact-btn"
-//                           type="submit"
-//                           disabled={loading}
-//                         >
-//                           <span className="contact-btn-text">
-//                             {' '}
-//                             {loading ? (
-//                               <Spinner as="span" animation="border" size="sm" />
-//                             ) : (
-//                               buttonText
-//                             )}
-//                           </span>
-//                         </button>
-//                       </Col>
-//                     </Row>
-//                   </form>
-//                 </div>
-//               )}
-//             </TrackVisibility>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </section>
-//   );
-// };
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Row, Col, Spinner } from 'react-bootstrap';
@@ -195,7 +33,7 @@ export const Contact = () => {
   };
 
   const validatePhone = (phone) => {
-    const phoneRegex = /^\d{10}$/;
+    const phoneRegex = /^\+38\d{10}$/;
     return phoneRegex.test(phone);
   };
 
@@ -214,7 +52,6 @@ export const Contact = () => {
       return;
     }
 
-    // Валидация email
     if (!validateEmail(formDetails.email)) {
       toast.error('Введіть дійсний email латиницею.', {
         containerId: 'main-toast',
@@ -222,7 +59,6 @@ export const Contact = () => {
       return;
     }
 
-    // Валидация номера телефона
     if (!validatePhone(formDetails.phone)) {
       toast.error('Введіть дійсний номер телефону у форматі +38XXXXXXXXXX.', {
         containerId: 'main-toast',
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,6 +33,97 @@ export const Footer = () => {
   };
 
   return (
+    // <footer className="footer">
+    //   <Container>
+    //     <div className="footer-wrap">
+    //       <img className="navbar-logo" src={crown} alt="logo" />
+    //       <h3>BEERKING</h3>
+    //       <ul className="footer-list">
+    //         <li className="footer-item">
+    //           <div className="footer-social">
+    //             <form id="subscribe-form" onSubmit={handleSubmit}>
+    //               <label htmlFor="email" className="label-text">
+    //                 Підписуйтесь на новини
+    //               </label>
+    //               <div className="footer-form">
+    //                 <input
+    //                   type="email"
+    //                   id="email"
+    //                   name="email"
+    //                   value={email}
+    //                   onChange={(e) => setEmail(e.target.value)}
+    //                   placeholder="Введіть Ваш email"
+    //                   required
+    //                   className="footer-input"
+    //                 />
+    //                 <button
+    //                   type="submit"
+    //                   disabled={loading}
+    //                   className="footer-btn"
+    //                 >
+    //                   <span className="navbar-btn-text">
+    //                     {loading ? (
+    //                       <Spinner as="span" animation="border" size="sm" />
+    //                     ) : (
+    //                       'Підписатися'
+    //                     )}
+    //                   </span>
+    //                 </button>
+    //               </div>
+    //             </form>
+    //           </div>
+    //         </li>
+    //         <li className="footer-item">
+    //           <div className="footer-social">
+    //             <p className="footer-text">Ми в соцмережах</p>
+    //             <div className="footer-social-icon">
+    //               <a
+    //                 href="https://web.telegram.org/k/"
+    //                 target="_blank"
+    //                 rel="noreferrer"
+    //               >
+    //                 <img src={telegram} alt="telegram" />
+    //               </a>
+
+    //               <a
+    //                 href="https://www.viber.com/ua/"
+    //                 target="_blank"
+    //                 rel="noreferrer"
+    //               >
+    //                 <img src={viber} alt="viber" />
+    //               </a>
+    //               <a
+    //                 href="https://www.instagram.com/beer_king_market?igsh=MTJmZHRlMmd1Y2Ywag=="
+    //                 target="_blank"
+    //                 rel="noreferrer"
+    //               >
+    //                 <img src={instagram} alt="instagram" />
+    //               </a>
+    //             </div>
+    //           </div>
+    //         </li>
+    //         <li className="footer-item">
+    //           <div className="footer-section">
+    //             <p className="footer-text">
+    //               <a href="/about" className="footer-link">
+    //                 Про Нас
+    //               </a>
+    //             </p>
+    //             <p className="footer-text">
+    //               <a href="/privacy-policy" className="footer-link">
+    //                 Політика Конфіденційності
+    //               </a>
+    //             </p>
+    //           </div>
+    //         </li>
+    //       </ul>
+    //       <p className="footer-text-right">
+    //         {' '}
+    //         CopyRight 2024. All Rights Reserved
+    //       </p>
+    //     </div>
+    //   </Container>
+    // </footer>
     <footer className="footer">
       <Container>
         <div className="footer-wrap">
