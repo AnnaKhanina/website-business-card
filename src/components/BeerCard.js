c7904abd5a8a245e1081b3496bdd91848cca066e
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-06-22 18:25:59 +0300

    fix about

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,45 +1,3 @@
-// import { Col } from 'react-bootstrap';
-// import crown from '../assets/img/icons/crown.png';
-// import glass from '../assets/img/icons/glass.png';
-// import barley from '../assets/img/icons/barley.png';
-
-// export const BeerCard = ({
-//   title,
-//   beerTitle,
-//   beerStrength,
-//   beerDensity,
-//   beerFactory,
-//   description,
-//   imgUrl,
-// }) => {
-//   return (
-//     <Col size={12} sm={6} md={4}>
-//       <div className="beer-imgbx" tabIndex="0">
-//         <img src={imgUrl} alt={title} />
-//         <p className="beer-title">{beerTitle}</p>
-//         <p>
-//           <span className="strength">Міцність: </span>
-//           <span className="strength-beer">{beerStrength}. </span>
-//           <span className="density">Щільність: </span>
-//           <span className="density-beer">{beerDensity}</span>
-//         </p>
-//         <p className="beer-factory">
-//           <span className="factory">Виробник: </span>
-//           <span className="factory-beer">{beerFactory}</span>
-//         </p>
-//         <div className="beer-txtx">
-//           <h4>{title}</h4>
-//           <p className="beer-discr">{description}</p>
-//           <img className="products-icon" src={crown} alt="crown" />
-//           <img className="products-icon" src={glass} alt="glass of Beer" />
-//           <img className="products-icon" src={barley} alt="malt" />
-//         </div>
-//       </div>
-//     </Col>
-//   );
-// };
-
-import React from 'react';
 import { Col } from 'react-bootstrap';
 import crown from '../assets/img/icons/crown.png';
 import glass from '../assets/img/icons/glass.png';
@@ -53,15 +11,10 @@ export const BeerCard = ({
   beerFactory,
   description,
   imgUrl,
-  isActive,
-  onClick,
 }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div
-        className={`beer-imgbx ${isActive ? 'active' : ''}`}
-        onClick={onClick}
-      >
+      <div className="beer-imgbx" tabIndex="0">
         <img src={imgUrl} alt={title} />
         <p className="beer-title">{beerTitle}</p>
         <p>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,94 +1,3 @@
-// import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
-// import { BeerCard } from './BeerCard';
-// import { CyderCard } from './CyderCard';
-// import { WineCard } from './WineCard';
-// import { beers } from '../products/beers';
-// import { cyders } from '../products/cyders';
-// import { wines } from '../products/wines';
-// import { ModalWindow } from '../components/Modal';
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
-
-// export const Products = () => {
-//   return (
-//     <section className="products" id="products">
-//       <Container>
-//         <Row>
-//           <Col size={12} className="products-col">
-//             <TrackVisibility>
-//               {({ isVisible }) => (
-//                 <div
-//                   className={
-//                     isVisible ? 'animate__animated animate__fadeIn' : ''
-//                   }
-//                 >
-//                   <h2>Продукція</h2>
-//                   <h3>Наш асортимент</h3>
-//                   <Tab.Container id="products-tabs" defaultActiveKey="first">
-//                     <Nav
-//                       variant="pills"
-//                       className="nav-pills mb-5 justify-content-center align-items-center"
-//                       id="pills-tab"
-//                     >
-//                       <Nav.Item>
-//                         <Nav.Link eventKey="first">Пиво</Nav.Link>
-//                       </Nav.Item>
-//                       <Nav.Item>
-//                         <Nav.Link eventKey="second">Сидр</Nav.Link>
-//                       </Nav.Item>
-//                       <Nav.Item>
-//                         <Nav.Link eventKey="third">Вино</Nav.Link>
-//                       </Nav.Item>
-//                     </Nav>
-//                     <Tab.Content
-//                       id="slideInUp"
-//                       className={
-//                         isVisible ? 'animate__animated animate__slideInUp' : ''
-//                       }
-//                     >
-//                       <Tab.Pane eventKey="first">
-//                         <Row>
-//                           {beers.map((beer, index) => {
-//                             return <BeerCard key={index} {...beer} />;
-//                           })}
-//                         </Row>
-//                       </Tab.Pane>
-//                       <Tab.Pane eventKey="second">
-//                         <Row>
-//                           {cyders.map((cyder, index) => {
-//                             return <CyderCard key={index} {...cyder} />;
-//                           })}
-//                         </Row>
-//                       </Tab.Pane>
-//                       <Tab.Pane eventKey="third">
-//                         <Row>
-//                           {wines.map((wine, index) => {
-//                             return <WineCard key={index} {...wine} />;
-//                           })}
-//                         </Row>
-//                       </Tab.Pane>
-//                     </Tab.Content>
-//                   </Tab.Container>
-//                 </div>
-//               )}
-//             </TrackVisibility>
-//             <p className="products-text">
-//               *Ассортимент може змінюватись. Актуальність цін та наявність
-//               товару можна дізнатися{' '}
-//               <a href="tel:+380730744434" className="products-tel">
-//                 за номером телефона
-//               </a>{' '}
-//               або в наших соцмережах.
-//             </p>
-//             <ModalWindow />
-//           </Col>
-//         </Row>
-//       </Container>
-//     </section>
-//   );
-// };
-
-import React, { useState } from 'react';
 import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
 import { BeerCard } from './BeerCard';
 import { CyderCard } from './CyderCard';
@@ -101,14 +10,6 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Products = () => {
-  const [activeCard, setActiveCard] = useState(null);
-
-  const handleCardClick = (index) => {
-    setActiveCard((prevActiveCard) =>
-      prevActiveCard === index ? null : index
-    );
-  };
-
   return (
     <section className="products" id="products">
       <Container>
@@ -147,47 +48,23 @@ export const Products = () => {
                     >
                       <Tab.Pane eventKey="first">
                         <Row>
-                          {beers.map((beer, index) => (
-                            <BeerCard
-                              key={index}
-                              {...beer}
-                              isActive={activeCard === index}
-                              onClick={() => handleCardClick(index)}
-                            />
-                          ))}
+                          {beers.map((beer, index) => {
+                            return <BeerCard key={index} {...beer} />;
+                          })}
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
                         <Row>
-                          {cyders.map((cyder, index) => (
-                            <CyderCard
-                              key={index}
-                              {...cyder}
-                              isActive={activeCard === index + beers.length}
-                              onClick={() =>
-                                handleCardClick(index + beers.length)
-                              }
-                            />
-                          ))}
+                          {cyders.map((cyder, index) => {
+                            return <CyderCard key={index} {...cyder} />;
+                          })}
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
                         <Row>
-                          {wines.map((wine, index) => (
-                            <WineCard
-                              key={index}
-                              {...wine}
-                              isActive={
-                                activeCard ===
-                                index + beers.length + cyders.length
-                              }
-                              onClick={() =>
-                                handleCardClick(
-                                  index + beers.length + cyders.length
-                                )
-                              }
-                            />
-                          ))}
+                          {wines.map((wine, index) => {
+                            return <WineCard key={index} {...wine} />;
+                          })}
                         </Row>
                       </Tab.Pane>
                     </Tab.Content>
