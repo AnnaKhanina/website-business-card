22037ae37114ade68bff20ee5a208732cf84533b
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-07-04 15:57:02 +0300

    done QR-code

--- src/App.js
+++ src/App.js
@@ -10,28 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ScrollToTopButton from './components/ScrollToTopButton';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useRef } from 'react';
-import QRCode from 'qrcode.react';
-import { toPng } from 'html-to-image';
 
 function App() {
-  const qrRef = useRef();
-
-  const downloadQRCode = () => {
-    if (qrRef.current) {
-      toPng(qrRef.current)
-        .then((dataUrl) => {
-          const link = document.createElement('a');
-          link.href = dataUrl;
-          link.download = 'qrcode.png';
-          link.click();
-        })
-        .catch((err) => {
-          console.error('Ошибка при создании QR-кода', err);
-        });
-    }
-  };
-
   return (
     <div className="App">
       <NavBar />
@@ -39,14 +19,6 @@ function App() {
       <About />
       <Partners />
       <Contact />
-      <div>
-        <h1>QR-код для перехода на сайт</h1>
-
-        <div ref={qrRef}>
-          <QRCode value="https://beerking-khm.vercel.app/" />
-        </div>
-        <button onClick={downloadQRCode}>Завантажити QR-код</button>
-      </div>
       <Footer />
       <ScrollToTopButton />
       <ToastContainer containerId="main-toast" />
