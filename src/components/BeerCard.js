ca202598a32a6bffde3f81b2efc1cfd8f33e76bc
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2024-07-04 13:20:32 +0300

    test overlay

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,3 +1,45 @@
+// import { Col } from 'react-bootstrap';
+// import crown from '../assets/img/icons/crown.png';
+// import glass from '../assets/img/icons/glass.png';
+// import barley from '../assets/img/icons/barley.png';
+
+// export const BeerCard = ({
+//   title,
+//   beerTitle,
+//   beerStrength,
+//   beerDensity,
+//   beerFactory,
+//   description,
+//   imgUrl,
+// }) => {
+//   return (
+//     <Col size={12} sm={6} md={4}>
+//       <div className="beer-imgbx" tabIndex="0">
+//         <img src={imgUrl} alt={title} />
+//         <p className="beer-title">{beerTitle}</p>
+//         <p>
+//           <span className="strength">Міцність: </span>
+//           <span className="strength-beer">{beerStrength}. </span>
+//           <span className="density">Щільність: </span>
+//           <span className="density-beer">{beerDensity}</span>
+//         </p>
+//         <p className="beer-factory">
+//           <span className="factory">Виробник: </span>
+//           <span className="factory-beer">{beerFactory}</span>
+//         </p>
+//         <div className="beer-txtx">
+//           <h4>{title}</h4>
+//           <p className="beer-discr">{description}</p>
+//           <img className="products-icon" src={crown} alt="crown" />
+//           <img className="products-icon" src={glass} alt="glass of Beer" />
+//           <img className="products-icon" src={barley} alt="malt" />
+//         </div>
+//       </div>
+//     </Col>
+//   );
+// };
+
+import React, { useState } from 'react';
 import { Col } from 'react-bootstrap';
 import crown from '../assets/img/icons/crown.png';
 import glass from '../assets/img/icons/glass.png';
@@ -12,9 +54,21 @@ export const BeerCard = ({
   description,
   imgUrl,
 }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="beer-imgbx" tabIndex="0">
+      <div
+        className="beer-imgbx"
+        tabIndex="0"
+        onMouseEnter={() => setShowDescription(true)}
+        onMouseLeave={() => setShowDescription(false)}
+        onClick={toggleDescription}
+      >
         <img src={imgUrl} alt={title} />
         <p className="beer-title">{beerTitle}</p>
         <p>
@@ -27,13 +81,15 @@ export const BeerCard = ({
           <span className="factory">Виробник: </span>
           <span className="factory-beer">{beerFactory}</span>
         </p>
-        <div className="beer-txtx">
-          <h4>{title}</h4>
-          <p className="beer-discr">{description}</p>
-          <img className="products-icon" src={crown} alt="crown" />
-          <img className="products-icon" src={glass} alt="glass of Beer" />
-          <img className="products-icon" src={barley} alt="malt" />
-        </div>
+        {showDescription && (
+          <div className="beer-txtx">
+            <h4>{title}</h4>
+            <p className="beer-discr">{description}</p>
+            <img className="products-icon" src={crown} alt="crown" />
+            <img className="products-icon" src={glass} alt="glass of Beer" />
+            <img className="products-icon" src={barley} alt="malt" />
+          </div>
+        )}
       </div>
     </Col>
   );
